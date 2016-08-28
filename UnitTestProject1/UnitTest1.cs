cc57234707c537c3d6512e3796157b018a3e1fae
cmusman2 <cmusman@hotmail.com>
2016-08-28 19:43:33 +0100

    unit tests added

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int test = 2;
+            Assert.AreEqual(2, test);
+        }
+
+        [TestMethod]
+        public async void TestMethod2()
+        {
+            string test = "test user";
+
+            ServiceReference1.Service1Client service = new ServiceReference1.Service1Client();
+            ServiceReference1.AuthorResponse auth = await service.GetInfoAsync(test);
+            
+            Assert.AreEqual("test user", test);
+        }
+    }
+}
